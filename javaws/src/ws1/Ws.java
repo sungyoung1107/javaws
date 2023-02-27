2becce542e45ded994718030b19ca5670f79a94c
jhs4132@gmail.com <jhs4132@gmail.com>
2023-02-27 17:25:56 +0900

    230227 hyeonson a3

--- javaws/src/ws1/Ws.java
+++ javaws/src/ws1/Ws.java
@@ -1,11 +1,14 @@
 package ws1;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ws {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Random r = new Random();
 		
 		while(true) {
 			System.out.println("Input command[q, a1, a2, a3, a4]");
@@ -28,7 +31,43 @@ public class Ws {
 				// 배열 정보를 출력 한다.
 				// 짝수 번째 숫자들의 합과 평균을 출력한다.
 				
+				
+				
+				
+				
 			}else if(cmd.equals("a3")){
+				while(true) {
+				System.out.println("5~9까지 숫자를 입력하세요.");
+				int cmd3 = Integer.parseInt(sc.next());
+				
+				if(!(cmd3>=5 && cmd3<=9)) {
+					System.out.println("다시 입력하세요.");
+					continue;
+				}else {
+				
+				int[] arr = new int[cmd3];
+				for(int i=0; i<arr.length; i++) {
+				 arr[i] = r.nextInt(9)+1;
+				}
+				
+				System.out.println(Arrays.toString(arr));
+				
+				int sum = 0, cnt = 0;
+				double avg = 0.0;
+				
+				for(int i=0; i<arr.length; i++) {
+					if(arr[i]%2==0) {
+						sum += arr[i];
+						cnt++;
+					}
+				}
+				avg = (sum*1.0)/cnt;
+				
+				System.out.println(sum);
+				System.out.println(avg);
+				break;}
+				}
+			
 				// 5~9까지 숫자를 입력 받는다.
 				// 입력 받은 숫자만큼 배열을 생성 하고
 				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.

69848bdd99f925a99af34fc8e55fd1b843f23aea
sungyoung1107 <dong09161107@gmail.com>
2023-02-27 16:15:27 +0900

    sungyoung 0227 hw

--- javaws/src/ws1/Ws.java
+++ javaws/src/ws1/Ws.java
@@ -8,20 +8,40 @@ public class Ws {
 		Scanner sc = new Scanner(System.in);
 		
 		while(true) {
-			System.out.println("Input command[q, a1, a2, a3, a4, a5 s]");
+			System.out.println("Input command[q, a1, a2, a3, a4]");
 			String cmd = sc.next();
 			
 			if(cmd.equals("q")) {
 				System.out.println("Quit..");
 				break;
 			}else if(cmd.equals("a1")){
-				System.out.println("");
+				// 5~9까지 숫자를 입력 받는다.
+				// 입력 받은 숫자만큼 배열을 생성 하고
+				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
+				// 배열 정보를 출력 한다.
+				// 배열의 합과 평균을 출력한다.
+				
 			}else if(cmd.equals("a2")){
-				System.out.println("");
+				// 5~9까지 숫자를 입력 받는다.
+				// 입력 받은 숫자만큼 배열을 생성 하고
+				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
+				// 배열 정보를 출력 한다.
+				// 짝수 번째 숫자들의 합과 평균을 출력한다.
+				
 			}else if(cmd.equals("a3")){
-				System.out.println("");
+				// 5~9까지 숫자를 입력 받는다.
+				// 입력 받은 숫자만큼 배열을 생성 하고
+				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
+				// 배열 정보를 출력 한다.
+				// 짝수 값만의 합과 평균을 출력한다.
+				
 			}else if(cmd.equals("a4")){
-				System.out.println("");
+				// 5~9까지 숫자를 2개 입력 받는다.
+				// 2차원 배열을 만든다.
+				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
+				// 배열 정보를 출력 한다.
+				// 배열의 합과 평균을 출력한다.
+				
 			}else {
 				System.out.println("Invalid Command..");
 				System.out.println("Try Again..");
